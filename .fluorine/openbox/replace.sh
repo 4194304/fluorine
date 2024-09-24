c4a9676de54fdd5280fde29b8a887aa9e30c31a8
4194304 <130800536+4194304@users.noreply.github.com>
2024-09-24 05:10:45 +0000

    openbox-25 apparently broke rc.xml, and I redid it

--- .fluorine/openbox/replace.sh
+++ .fluorine/openbox/replace.sh
@@ -2,4 +2,3 @@
 cd ~/.fluorine/openbox
 cp ~/.config/openbox/rc.xml ~/.fluorine/openbox/rc.xml
 sed "/ this stuff/ {r openbox-$1"$'\n'"} ; /theme/,/ this stuff/ {d}" rc.xml > ~/.config/openbox/rc.xml
-rm ~/.fluorine/openbox/rc.xml
