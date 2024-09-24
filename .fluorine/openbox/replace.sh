eb40adeefcaa50108ca32531d6ed81ee69a51562
4194304 <130800536+4194304@users.noreply.github.com>
2024-09-24 04:16:54 +0000

    Add files via upload

---
+++ .fluorine/openbox/replace.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+cd ~/.fluorine/openbox
+cp ~/.config/openbox/rc.xml ~/.fluorine/openbox/rc.xml
+sed "/ this stuff/ {r openbox-$1"$'\n'"} ; /theme/,/ this stuff/ {d}" rc.xml > ~/.config/openbox/rc.xml
+rm ~/.fluorine/openbox/rc.xml
