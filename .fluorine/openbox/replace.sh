b866c9a1dfb382ea7f234431ef67759d82ad3625
4194304 <130800536+4194304@users.noreply.github.com>
2024-09-27 02:07:02 +0000

    it didn't work (oops)

--- .fluorine/openbox/replace.sh
+++ .fluorine/openbox/replace.sh
@@ -2,4 +2,4 @@
 cd ~/.fluorine/openbox
 rm rc2.xml
 cp ~/.config/openbox/rc.xml ~/.fluorine/openbox/rc.xml
-sed "/ <!-- openboxclose -->/ {r openbox-$1"$'\n'"} ; /<!-- openboxstart -->/,/ <!-- openboxclose -->/ {d}" ~/.fluorine/openbox/rc.xml > ~/.config/openbox/rc.xml
+sed "/<!-- openboxclose -->/ {r openbox-$1"$'\n'"} ; /<!-- openboxstart -->/,/<!-- openboxclose -->/ {d}" ~/.fluorine/openbox/rc.xml > ~/.config/openbox/rc.xml
