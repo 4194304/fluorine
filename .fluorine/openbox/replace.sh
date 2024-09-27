61b98a9078977837bbebb027b1bb4794f43892d8
4194304 <130800536+4194304@users.noreply.github.com>
2024-09-27 01:54:49 +0000

    I don't know if this will work or not but who cares

--- .fluorine/openbox/replace.sh
+++ .fluorine/openbox/replace.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/bash
 cd ~/.fluorine/openbox
+rm rc2.xml
 cp ~/.config/openbox/rc.xml ~/.fluorine/openbox/rc.xml
-sed "/ this stuff/ {r openbox-$1"$'\n'"} ; /theme/,/ this stuff/ {d}" rc.xml > ~/.config/openbox/rc.xml
+sed "/ <!-- openboxclose -->/ {r openbox-$1"$'\n'"} ; /<!-- openboxstart -->/,/ <!-- openboxclose -->/ {d}" ~/.fluorine/openbox/rc.xml > ~/.config/openbox/rc.xml
