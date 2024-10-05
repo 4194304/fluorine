21c3fb493711e266b5e50938988c43351eb790a5
4194304 <130800536+4194304@users.noreply.github.com>
2024-10-05 00:10:18 +0000

    long time no see
    now allows you to apply your desired scale factor in your session without restarting

--- .fluorine/setinterface.sh
+++ .fluorine/setinterface.sh
@@ -3,4 +3,15 @@ cp ~/.fluorine/tint2/tint2rc-$1 ~/.config/tint2/tint2rc
 cp ~/.fluorine/jgmenu/jgmenurc-$1 ~/.config/jgmenu/jgmenurc
 ~/.fluorine/./setdpi.sh $1 $1
 ~/.fluorine/openbox/./replace.sh $1
+echo "Restarting interface elements..." & {
+killall tint2
+killall jgmenu
+tint2 & disown
+~/.fluorine/dpi.sh
+openbox --reconfigure
+killall opensnap
+opensnap & disown
+} &> /dev/null
+echo " "
+echo "If any GTK or Qt applications are open, please restart them for your new scale factor to take effect."
 exit
