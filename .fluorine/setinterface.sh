26832246c0a1556c2f87aa4fb3ed0f5b3c1afcd9
4194304 <130800536+4194304@users.noreply.github.com>
2024-10-07 03:13:11 +0000

    Update setinterface.sh

--- .fluorine/setinterface.sh
+++ .fluorine/setinterface.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 cp ~/.fluorine/tint2/tint2rc-$1 ~/.config/tint2/tint2rc
 cp ~/.fluorine/jgmenu/jgmenurc-$1 ~/.config/jgmenu/jgmenurc
-~/.fluorine/./setdpi.sh $1 $1
+~/.fluorine/setdpi.sh $1
 ~/.fluorine/openbox/./replace.sh $1
 echo "Restarting interface elements..." & {
 xrdb -merge ~/.Xresources
 killall tint2
 killall jgmenu
 tint2 & disown
-~/.fluorine/dpi.sh
 openbox --reconfigure
 killall opensnap
 opensnap & disown
