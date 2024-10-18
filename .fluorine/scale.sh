2fd064255c23378556194b106c8709537af67865
4194304 <130800536+4194304@users.noreply.github.com>
2024-10-18 05:02:43 +0000

    script became destructive oops

--- .fluorine/scale.sh
+++ .fluorine/scale.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
+if ! [[ "$1" =~ ^[0-9]+$ ]]
+    then
+        echo "Number is not a valid integer, try again."
+        exit
+fi
+
+rm -rf ~/.Xresources
+N=$(bc -l <<< '100/24')
+C=$(bc -l <<< $1/$N)
+D=$(printf "%.0f\n" "$C")
+declare -i A=($1*96/100)
+echo Xft.dpi: $A > ~/.Xresources
+echo "Xcursor.size: $D" >> ~/.Xresources
+rm -rf ~/.fluorine/dpi.sh
+
+
+cd ~
 mkdir -p ~/.fluorine/temp/
 cp ~/.config/gtk-3.0/settings.ini ~/.fluorine/temp/settings.ini
 cp ~/.fluorine/openbox/openbox-100 ~/.fluorine/temp/openbox-custom
@@ -21,7 +38,9 @@ if (($u > 26));
 	then
 		sed -i -e "s/lowDPI font 17/Cantarell 11/g" ~/.fluorine/temp/settings.ini
 fi
-cd ~/.fluorine/temp
-sed $'/ <!-- openboxclose -->/ {r openbox-custom\n} ; /<!-- openboxstart -->/,/<!-- openboxclose -->/ {d}' ~/.fluorine/temp/rc.xml > ~/.config/openbox/rc.xml
+
+cd ~/.fluorine/temp/
+sed $'/<!-- openboxclose -->/ {r openbox-custom\n} ; /<!-- openboxstart -->/,/<!-- openboxclose -->/ {d}' ~/.fluorine/temp/rc.xml > ~/.config/openbox/rc.xml
 cd ~
 rm -rf ~/.fluorine/temp/
+openbox --reconfigure
