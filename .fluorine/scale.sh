7cba7f2a83af25b1502a007d80313934e6d4ef12
4194304 <130800536+4194304@users.noreply.github.com>
2024-10-18 04:50:46 +0000

    one new script to soon deprecate them all

---
+++ .fluorine/scale.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+mkdir -p ~/.fluorine/temp/
+cp ~/.config/gtk-3.0/settings.ini ~/.fluorine/temp/settings.ini
+cp ~/.fluorine/openbox/openbox-100 ~/.fluorine/temp/openbox-custom
+cp ~/.config/openbox/rc.xml ~/.fluorine/temp/rc.xml
+p=$1
+q=$(awk '{for (i=1; i<=NF; i++) $i = int( ($i+12) / 25) * 25} 1' <<< $p)
+r=$(bc -l <<< "$q/100")
+s=$(printf "%.*f\n" 2 $r)
+sed -i -e "s/1x/"$s"x/g" ~/.fluorine/temp/openbox-custom
+t=$(bc <<< "$s*100")
+u=$(printf "%.*f\n" 0 $t)
+if (($u < 26));
+	then
+		sed -i -e "s/11/17/g" ~/.fluorine/temp/openbox-custom
+		sed -i -e "s/13/17/g" ~/.fluorine/temp/openbox-custom
+		sed -i -e "s/Cantarell/lowDPI font/g" ~/.fluorine/temp/openbox-custom
+		sed -i -e "s/Cantarell 11/lowDPI font 17/g" ~/.fluorine/temp/settings.ini
+fi
+if (($u > 26));
+	then
+		sed -i -e "s/lowDPI font 17/Cantarell 11/g" ~/.fluorine/temp/settings.ini
+fi
+cd ~/.fluorine/temp
+sed $'/ <!-- openboxclose -->/ {r openbox-custom\n} ; /<!-- openboxstart -->/,/<!-- openboxclose -->/ {d}' ~/.fluorine/temp/rc.xml > ~/.config/openbox/rc.xml
+cd ~
+rm -rf ~/.fluorine/temp/
