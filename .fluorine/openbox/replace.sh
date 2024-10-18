96502c256357f630efba5e8e158b58e12c9665d9
4194304 <130800536+4194304@users.noreply.github.com>
2024-10-18 04:50:05 +0000

    Delete .fluorine/openbox/replace.sh

--- .fluorine/openbox/replace.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd ~/.fluorine/openbox
-cp ~/.config/openbox/rc.xml ~/.fluorine/openbox/rc.xml
-sed "/<!-- openboxclose -->/ {r openbox-$1"$'\n'"} ; /<!-- openboxstart -->/,/<!-- openboxclose -->/ {d}" ~/.fluorine/openbox/rc.xml > ~/.config/openbox/rc.xml
