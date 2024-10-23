79a4db1795c34fe059e827621faf8868592cb4d3
4194304 <130800536+4194304@users.noreply.github.com>
2024-10-23 02:25:20 +0000

    Delete .fluorine/lowdpi directory

--- .fluorine/lowdpi/lowdpi.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sleep 0.1
-cp ~/.fluorine/lowdpi/l3afpadrc ~/.config/l3afpad/l3afpadrc
-cp ~/.fluorine/lowdpi/lxterminal.conf ~/.config/lxterminal/lxterminal.conf
-cp ~/.fluorine/lowdpi/settings.ini ~/.config/gtk-3.0/settings.ini
--- .fluorine/lowdpi/normal/normal.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sleep 0.1
-cp ~/.fluorine/lowdpi/normal/l3afpadrc ~/.config/l3afpad/l3afpadrc
-cp ~/.fluorine/lowdpi/normal/lxterminal.conf ~/.config/lxterminal/lxterminal.conf
-cp ~/.fluorine/lowdpi/normal/settings.ini ~/.config/gtk-3.0/settings.ini
